48bc6a7870d67e33da900d0d8ff4482c64d50b19
Mariska Magyar <mariska@t-shaped.me>
2020-12-01 00:20:49 +0100

    feat(store): add store

---
+++ toplist/src/store/index.js
@@ -0,0 +1,19 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { connectRouter } from 'connected-react-router';
+import container from '../container';
+
+function Store(history) {
+  const store = configureStore({
+    reducer: {
+      router: connectRouter(history),
+      movies: container.movieState.store,
+    },
+    devTools: true,
+  });
+  return store;
+}
+
+Store.$inject = ['history'];
+Store.$name = 'Store';
+Store.$type = 'service';
+export default Store;
