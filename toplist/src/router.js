0e793820295c8140fde32500e7487ceaa6c77879
Mariska Magyar <mariska@t-shaped.me>
2020-12-01 01:01:38 +0100

    feat(pages/MovieList): add new page
    This page lists all the basic information about the movies.
    It would need some more refactoring. A basic component
    could be created to avoid code duplication later on,
    but I run out of time sadly.

--- toplist/src/router.js
+++ toplist/src/router.js
@@ -2,13 +2,20 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import { ConnectedRouter } from 'connected-react-router';
 import Main from './pages/main';
+import MovieList from './pages/MovieList';
 import container from './container';
 
 const Router = () => {
   return (
     <ConnectedRouter history={container.history}>
       <Main>
-        <Switch/>
+        <Switch>
+          <Route
+            exact
+            path="/movies/"
+            render={(props) => <MovieList {...props} />}
+          />
+        </Switch>
       </Main>
     </ConnectedRouter>
   );
